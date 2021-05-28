83353e368bb9363daf998f9b32092ecc5921a36b
NN <admin@aishitei.ru>
2021-05-27 22:47:50 -0700

    Remove html-minifier since it removes integrity and replace with gulp-htmlnano
    Signed-off-by: NN <admin@aishitei.ru>

--- gulpfile.js
+++ gulpfile.js
@@ -1,8 +1,12 @@
-const { src, dest } = require('gulp');
+const { gulp, src, dest } = require('gulp');
+const htmlnano = require('gulp-htmlnano');
 const sriHash = require('gulp-sri-hash');
 
 sriHash();
 
 exports.default = function () {
-	return src('./src/*.html').pipe(sriHash()).pipe(dest('./public'));
+	return src('./src/*.html')
+		.pipe(htmlnano())
+		.pipe(sriHash())
+		.pipe(dest('./public'));
 };
