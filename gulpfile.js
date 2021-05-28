79c68c1e4a3e25e323423fd7efd2dd700ae74650
NN <admin@aishitei.ru>
2021-05-27 22:54:29 -0700

    Updates gulpfile settings for htmlnano
    Signed-off-by: NN <admin@aishitei.ru>

--- gulpfile.js
+++ gulpfile.js
@@ -2,11 +2,16 @@ const { gulp, src, dest } = require('gulp');
 const htmlnano = require('gulp-htmlnano');
 const sriHash = require('gulp-sri-hash');
 
+const nanoOptions = {
+	removeComments: false,
+	collapseWhitespace: 'aggressive',
+};
+
 sriHash();
 
 exports.default = function () {
 	return src('./src/*.html')
-		.pipe(htmlnano())
+		.pipe(htmlnano(nanoOptions))
 		.pipe(sriHash())
 		.pipe(dest('./public'));
 };
